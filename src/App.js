0724df0d783ba5091cb04ebc6603e24f1cef4476
Aravindhthamizh0203 <aravindhthamizh0203@gmail.com>
2022-11-23 19:55:35 +0530

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const movie = [
     {
       name: "FREE",
-      cost: "0$/Month",
+      cost: "0$",
       des: "single user",
       des1: "5GB Storage",
       des2: "Unlimited Public Projects",
@@ -16,7 +16,7 @@ function App() {
       des7: "Monthly Status Reports"
     }, {
       name: "PLUS",
-      cost: "49$/Month",
+      cost: "49$",
       des: "5 user",
       des1: "50GB Storage",
       des2: "Unlimited Public Projects",
@@ -27,7 +27,7 @@ function App() {
       des7: "Monthly Status Reports"
     }, {
       name: "PRO",
-      cost: "149$/Month",
+      cost: "149$",
       des: "muilt user",
       des1: "150GB Storage",
       des2: "Unlimited Public Projects",
@@ -48,25 +48,20 @@ function App() {
             <h6 className="option">{user.des}</h6> */}
             <div key={i} className="user">
               <Usr name={user.name} cost={user.cost} des={user.des} des1={user.des1} des2={user.des2} des3={user.des3} des4={user.des4} des5={user.des5} des6={user.des6} des7={user.des7} />
-              <div class="d-grid">
-                <a href="#" class="button"><b>view more</b></a>
-              </div>
-
+              <Button />
             </div>
           </>
         )
       })}
-
-
-
     </div>
   )
 }
 function Usr({ name, cost, des, des1, des2, des3, des4, des5, des6, des7 }) {
   return (
     <div>
-      <h4 className="plan">{name}</h4>
-      <h2 className="prise">{cost}</h2>
+      <h3 className="plan">{name}</h3>
+      <h2 className="prise">{cost}/Month</h2>
+      <hr />
       <div className="option">{des}</div>
       <div className="option">{des1}</div>
       <div className="option">{des2}</div>
@@ -74,11 +69,14 @@ function Usr({ name, cost, des, des1, des2, des3, des4, des5, des6, des7 }) {
       <div className="option">{des4}</div>
       <div className="option">{des5}</div>
       <div className="option">{des6}</div>
-      <div className="option">{des7}</div>
-      {/* <div className="button">
-        <button type="button">view more</button>
-      </div> */}
-
+      <div className="option">{des7}</div><br />
+    </div>
+  )
+}
+function Button() {
+  return (
+    <div class="d-grid">
+      <a href="#" class="button"><b>view more</b></a>
     </div>
   )
 }
